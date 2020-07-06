d4801fb673b92d1d5e8ef7b015452531ea24e088
user <user@git.com>
2020-07-06 20:54:56 +0100

    updated get() to check wiringPi setup success and updated unit test

--- Sensors/nbproject/Package-Debug.bash
+++ Sensors/nbproject/Package-Debug.bash
@@ -87,6 +87,10 @@ cd "${TOP}"
 makeDirectory "${NBTMPDIR}/libSensors.so/lib"
 copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}lib/${OUTPUT_BASENAME}" 0644
 
+cd "${TOP}"
+makeDirectory "${NBTMPDIR}/libSensors.so/lib"
+copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}lib/${OUTPUT_BASENAME}" 0644
+
 
 # Generate tar file
 cd "${TOP}"
